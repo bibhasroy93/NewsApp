d5fb02dfc5a4776ee1852905f7d49da08bd4a259
bibhasroy93 <butunroy@gmail.com>
2022-01-28 00:08:39 +0530

    28012022

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,47 +1,69 @@
 import React,{useState,useEffect} from 'react';
-import { Text, View , FlatList, ActivityIndicator} from 'react-native';
+import { View , FlatList, ActivityIndicator} from 'react-native';
+import {
+  Card,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+  CardImage,
+} from "react-native-material-cards";
+const API_URL="https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd46bd6a4f54fe69d0cb261dbe1a878";
 
-
-const API_URL="?country=in&category=business&apiKey=cbd46bd6a4f54fe69d0cb261dbe1a878";
 const request = new Request(API_URL);
 function Home() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   
-    const getMovies = async () => {
-      try {
-       const response = await fetch(request,{
-         method:'GET',
-         headers :{
-          "Content-Type":"application/json"
-         }
-       });
-       const result = await response.json();
-       setData(result);
-       console.log(result)
-     } catch (error) {
-       console.error(error);
-     } finally {
-       setLoading(false);
-     }
+    
+  const getNews = async () => {
+    try {
+     const response = await fetch(API_URL);
+     const json = await response.json();
+     setData(json.articles);
+     //console.log(json);
+   } catch (error) {
+     console.error(error);
+   } finally {
+     setLoading(false);
    }
+ }
 
     useEffect(() => {
-      getMovies();
+      getNews();
     }, []);
 
  
     return (
-      <View style={{ flex: 1, padding: 24 }}>
-        {/* {isLoading ? <ActivityIndicator/> : (
+      <View style={{ flex: 1}}>
+        {isLoading ? <ActivityIndicator/> : (
           <FlatList
             data={data}
             keyExtractor={({ id }, index) => id}
             renderItem={({ item }) => (
-              <Text>{item.title}, {item.description}</Text>
+             
+              
+                  
+              <View >
+              <Card>
+                <CardImage
+                  source={{ uri: item.urlToImage }}
+                  title={data.name}
+                />
+                <CardTitle
+                  title={item.title}
+                  textStyle={{fontWeight:'bold'}}
+                />
+                <CardContent text={item.description}/>
+                <CardAction separator={true} inColumn={false}>
+                  <CardButton onPress={() => {}} title="Share" color="green" />
+                </CardAction>
+              </Card>
+            </View>
+              
             )}
           />
-        )} */}
+        )}
       </View>
     );
   }
--- Screens/SportsScreen.js
+++ Screens/SportsScreen.js
@@ -1,12 +1,70 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { View, FlatList, ActivityIndicator} from "react-native";
+import {
+  Card,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+  CardImage,
+} from "react-native-material-cards";
+const API_URL =
+  "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=cbd46bd6a4f54fe69d0cb261dbe1a878";
+const request = new Request(API_URL);
 function Sports() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Sports</Text>
-      </View>
-    );
-  }
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-  export default Sports
+  const getNews = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      setData(json.articles);
+      //console.log(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            
+
+            <View >
+              <Card>
+                <CardImage
+                  source={{ uri: item.urlToImage }}
+                  title={data.name}
+                />
+                <CardTitle
+                  title={item.title}
+                  textStyle={{fontWeight:'bold'}}
+                />
+                <CardContent text={item.description}/>
+                <CardAction separator={true} inColumn={false}>
+                  <CardButton onPress={() => {}} title="Share" color="green" />
+                </CardAction>
+              </Card>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+}
+
+export default Sports;
+
